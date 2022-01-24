c116dbf8592c05c8b189687a99740b24c336f93e
HyeongSeok Lim <98303348+NANEUNSUGI@users.noreply.github.com>
2022-01-24 17:07:06 +0900

    Add files via upload

---
+++ game2.py
@@ -0,0 +1,18 @@
+import random
+
+com = random.randint(1,99)
+
+count = 0
+while True:
+
+    count += 1
+    print(f"시도 횟수는{count}번입니다. ")
+    user = int(input('숫자를 입력하세요. '))
+
+    if user > com:
+        print('다운')
+    elif user < com:
+        print('업')
+    else:
+        print('정답입니다.')
+        break
